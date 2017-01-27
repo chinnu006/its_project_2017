c6dd7caffa54a8176d26e8f0d0f3236480097f13
Jagadeesh Ponduru <jagadeesh.etech@gmail.com>
2017-01-27 14:58:56 +0530

    Add files via upload
    Updated version for DashboardController.js

--- dashboardController.js
+++ dashboardController.js
@@ -143,4 +143,246 @@ app.controller('dashboardCtrl', function($scope){
             }
         });
 
+        // Create the chart
+      function detailedCharts () {
+        $scope.isDetailed = true;
+        var target1 = angular.element('.airQualityGraph').remove();
+        var target2 = angular.element('.waterQualityGraph').remove();
+        Highcharts.stockChart('detailed-chart1', {
+            chart: {
+                events: {
+                    load: function () {
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function () {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.round(Math.random() * 100);
+                            series.addPoint([x, y], true, true);
+                        }, 1000);
+                    }
+                }
+            },
+
+            rangeSelector: {
+                buttons: [{
+                    count: 1,
+                    type: 'minute',
+                    text: '1M'
+                }, {
+                    count: 5,
+                    type: 'minute',
+                    text: '5M'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false,
+                selected: 0
+            },
+
+            title: {
+                text: 'Live random data'
+            },
+
+            exporting: {
+                enabled: false
+            },
+
+            series: [{
+                name: 'Random data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -999; i <= 0; i += 1) {
+                        data.push([
+                            time + i * 1000,
+                            Math.round(Math.random() * 100)
+                        ]);
+                    }
+                    return data;
+                }())
+            }]
+        });
+
+
+
+        /********************************************
+        =================
+        *****************/
+        Highcharts.chart('detailed-chart2', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Contribution Values'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Sensor Parameters',
+                colorByPoint: true,
+                data: [{
+                    name: 'pH Value',
+                    y: 56.33
+                }, {
+                    name: 'Conductivity',
+                    y: 24.03,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Turbidity',
+                    y: 10.38
+                }, {
+                    name: 'Dissolved Oxigen',
+                    y: 4.77
+                }]
+            }]
+        });
+      
+        /**************************
+        ===========================
+        ***************************/
+
+
+      };
+
+      function detailedCharts1 () {
+        $scope.isDetailed = true;
+        var target1 = angular.element('.airQualityGraph').remove();
+        var target2 = angular.element('.waterQualityGraph').remove();
+        Highcharts.stockChart('detailed-chart1', {
+            chart: {
+                events: {
+                    load: function () {
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function () {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.round(Math.random() * 100);
+                            series.addPoint([x, y], true, true);
+                        }, 1000);
+                    }
+                }
+            },
+
+            rangeSelector: {
+                buttons: [{
+                    count: 1,
+                    type: 'minute',
+                    text: '1M'
+                }, {
+                    count: 5,
+                    type: 'minute',
+                    text: '5M'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false,
+                selected: 0
+            },
+
+            title: {
+                text: 'Live random data'
+            },
+
+            exporting: {
+                enabled: false
+            },
+
+            series: [{
+                name: 'Random data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -999; i <= 0; i += 1) {
+                        data.push([
+                            time + i * 1000,
+                            Math.round(Math.random() * 100)
+                        ]);
+                    }
+                    return data;
+                }())
+            }]
+        });
+
+
+
+        /********************************************
+        =================
+        *****************/
+        Highcharts.chart('detailed-chart2', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Contribution Values'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Sensor Parameters',
+                colorByPoint: true,
+                data: [{
+                    name: 'Humidity',
+                    y: 56.33
+                }, {
+                    name: 'Carbon Monoxide',
+                    y: 24.03,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Sulphur Dioxide',
+                    y: 10.38
+                }, {
+                    name: 'Ozone Monitoring',
+                    y: 4.77
+                }]
+            }]
+        });
+      
+        /**************************
+        ===========================
+        ***************************/
+
+
+      };
+
+
 });
